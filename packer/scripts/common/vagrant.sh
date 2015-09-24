bd5dd32528b739a51e760b42a4285bf1de9f2055
Alexander Birk <birk@pingworks.de>
2015-09-24 14:50:31 +0200

    Install insecure private key for user vagrant as well

--- packer/scripts/common/vagrant.sh
+++ packer/scripts/common/vagrant.sh
@@ -4,5 +4,9 @@ mkdir ~vagrant/.ssh
 wget --no-check-certificate \
     'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' \
     -O ~vagrant/.ssh/authorized_keys
+cp ~vagrant/.ssh/authorized_keys ~vagrant/.ssh/id_rsa.pub
+wget --no-check-certificate \
+    'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant' \
+    -O ~vagrant/.ssh/id_rsa
 chown -R vagrant ~vagrant/.ssh
 chmod -R go-rwsx ~vagrant/.ssh
